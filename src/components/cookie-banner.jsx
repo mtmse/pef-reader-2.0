23e8c91ffc4cfbfc9168ba221e35ba5c8fc97d8b
camillalandin <camilla.landin@mtm.se>
2024-10-07 09:59:04 +0200

    styld buttons in cookie-banner

--- src/components/cookie-banner.jsx
+++ src/components/cookie-banner.jsx
@@ -22,15 +22,15 @@ export default function CookieBanner({ setCookiePermission }) {
 
 
             <div className="flex justify-center w-full m-5">
-                <button onClick={() => setCookiePermission(CookieEnum.ALLOWED)}
-                    className="text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out mr-2 w-4/5
+               <button onClick={() => setCookiePermission(CookieEnum.ALLOWED)}
+                    className="flex-none w-64 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out mr-2 w-4/5
                     bg-gradient-to-b from-emerald-400 to-emerald-700   
                     hover:from-emerald-600 hover:to-emerald-800
                     focus:from-emerald-600 focus:to-emerald-800">
                     Godkänna nödvändiga kakor
                 </button>
                 <button onClick={() => setCookiePermission(CookieEnum.DENIED)}
-                    className="text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out w-1/5
+                    className="flex-none w-32 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out w-1/5
                     bg-gradient-to-b from-red-400 to-red-700   
                     hover:from-red-600 hover:to-red-800
                     focus:from-red-600 focus:to-red-800">
