637e7bca81271a552434a1030ac0f227ce8ed0c4
Camilla Landin <landin.camilla8@gmail.com>
2025-01-27 12:49:18 +0100

    removing usused vars and browserrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css'
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
