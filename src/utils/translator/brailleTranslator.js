dfa8fb36874be9b8f2ba23b0987d3da0a1ec5572
HenrikSigeman <henrik.sigeman@mtm.se>
2025-10-30 11:11:04 +0100

    Hotfix: Corrected behavior regarding braille symbols to be ignored and also lost letters following braille symbosl to be ignored.

--- src/utils/translator/brailleTranslator.js
+++ src/utils/translator/brailleTranslator.js
@@ -19,7 +19,7 @@ export default function brailleTranslator(braillePhrase) {
         let newChar // undefined
 
         if (isIgnoreSymbol(currentBrailleChar)) {
-            newPhrase += currentBrailleChar
+            // newPhrase += currentBrailleChar // why add braile symbols when ignored?
             continue
         }
 
@@ -37,8 +37,8 @@ export default function brailleTranslator(braillePhrase) {
             const doubleSymbols = currentBrailleChar + nextChar
 
             if (isIgnoreSymbol(doubleSymbols)) {
-                newPhrase += doubleSymbols
-                i += 2 
+                //newPhrase += doubleSymbols // why add braile symbols when ignored?
+                i += 1
                 continue
             }
 
@@ -47,7 +47,7 @@ export default function brailleTranslator(braillePhrase) {
 
             if (newChar) {
                 newPhrase += newChar
-                i += 2  
+                i += 1  
                 continue
             }
         }
